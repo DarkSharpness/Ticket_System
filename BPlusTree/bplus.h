677bcf8d01addbd45f288ecf3579ef541e845f9f
DarkSharpness_XzY <2040703891@qq.com>
2023-04-22 17:00:39 +0800

    Fuck I fix the bug of too small BLOCK_SIZE FUCK!!

--- BPlusTree/bplus.h
+++ BPlusTree/bplus.h
@@ -33,7 +33,7 @@ using val_comp = Compare   <T>;
 
 constexpr int TABLE_SIZE = 10000;
 constexpr int CACHE_SIZE = 100000; // NO LESS THAN tree_height * 2 + 2
-constexpr int BLOCK_SIZE = 9;
+constexpr int BLOCK_SIZE = 101;
 constexpr int AMORT_SIZE = BLOCK_SIZE * 2 / 3;
 constexpr int MERGE_SIZE = BLOCK_SIZE / 3;
 constexpr int  MAX_SIZE  = 300000;
